b23fbe4fdc9c140132a57ef801192dac5b6efdff
Ryan Leahy <ryan.leahy@pendo.io>
2018-03-27 15:30:22 -0500

    created app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="container">
+            <account-exec
+                :profile="execs[0]"
+            />
+            <account-exec
+                :profile="execs[1]"
+            />
+            <img
+                class="needle"
+                :class="nextUp.name"
+                src="./assets/gauge-needle.png">
+        </div>
+
+        <button
+            type="button"
+            name="switch ae"
+            @click="switchAe()">
+            Give next opp to {{nextUp.name}}
+        </button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AccountExec from './components/account-exec.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        AccountExec
+    },
+    data () {
+        return {
+            execs: [
+                {
+                    name: 'sandy',
+                    next: false,
+                    lastOpp: new Date()
+                },
+                {
+                    name: 'wilson',
+                    next: true,
+                    lastOpp: new Date()
+                },
+            ]
+        };
+    },
+    computed: {
+        nextUp () {
+            if (this.execs) {
+                let next = this.execs.filter((exec) => {
+                    return exec.next;
+                });
+
+                return next[0];
+            }
+        }
+    },
+    methods: {
+        switchAe () {
+            if (this.execs[0].next) {
+                this.execs[0].next = false;
+                this.execs[1].next = true;
+            } else {
+                this.execs[1].next = false;
+                this.execs[0].next = true;
+            }
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+
+    .container {
+        position: relative;
+        display: flex;
+        justify-content: center;
+        padding-bottom: 100px;
+    }
+
+    .needle {
+        position: absolute;
+        width: 30px;
+        bottom: 0;
+
+        &.sandy {
+            transform: rotate(-30deg);
+        }
+
+        &.wilson {
+            transform: rotate(30deg);
+        }
+    }
 }
 </style>
---
+++ src/components/account-exec.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="ae-profile">
+        <img class="ae-pic" :src="getSrc()">
+        <span v-if="!profile.next">{{oppDisplay}}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AccountExec',
+    props: {
+        profile: Object
+    },
+    data () {
+        return {
+            oppDisplay: `${this.profile.lastOpp.toDateString()} at ${this.profile.lastOpp.toLocaleTimeString()}`
+        };
+    },
+    methods: {
+        getSrc () {
+            return require(`../assets/${this.profile.name}.png`);
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .ae-profile {
+        display: flex;
+        flex-direction: column;
+
+        .ae-pic {
+            width: 100px;
+            margin: 0 30px
+        }
+    }
+</style>
